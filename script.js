057062f66e1584c22a8194f82ef67d76a73a5d70
thiaguinho08 <0000107482698xsp@al.educacao.sp.gov.br>
2026-04-22 15:36:01 -0300

    ..

---
+++ script.js
@@ -0,0 +1,12 @@
+let carrinho=[];
+function adicionarCarrinho(){
+carrinho.push({
+    nome:'Tênis Nike', preço:499
+})
+
+alert('Produto adicionado')
+}
+
+let verCarrinho=(){
+    window.location.href='carrinho.html'
+}
